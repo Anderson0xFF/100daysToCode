e4d269d432c6fb67433e80c45c507a2797678106
Anderson Almeida <andersonvsc1998@gmail.com>
2022-02-13 20:04:52 -0300

    day one

---
+++ day1/README.md
@@ -0,0 +1,3 @@
+  # Day 1
+
+Band name generator.
---
+++ day1/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator!")
+city = input("What city were you born in?\n")
+pet = input("Say the name of a pet.\n")
+print("Your band name is: " + city + " " + pet)
